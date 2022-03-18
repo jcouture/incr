d6511abe0fe1d9ef4d3eb3d60fa083415429d43d
Jean-Philippe Couture <jcouture@gmail.com>
2022-03-18 15:35:17 -0400

    Add a few more unit tests.

---
+++ spec/incr/service/version_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+require 'sem_version'
+
+describe Incr::Service::Version do
+  describe '.increment_segment' do
+    context 'with a SemVersion object' do
+      let(:version) { SemVersion.new('1.0.0') }
+
+      it 'should increment the major segment' do
+        expected = '2.0.0'
+        result = Incr::Service::Version.increment_segment(version, 'major')
+
+        expect(expected).to eql(result.to_s)
+      end
+
+      it 'should increment the minor segment' do
+        expected = '1.1.0'
+        result = Incr::Service::Version.increment_segment(version, 'minor')
+
+        expect(expected).to eql(result.to_s)
+      end
+
+      it 'should increment the patch segment' do
+        expected = '1.0.1'
+        result = Incr::Service::Version.increment_segment(version, 'patch')
+
+        expect(expected).to eql(result.to_s)
+      end
+    end
+
+    context 'with a version string' do
+      let(:version) { '1.0.0' }
+
+      it 'should throw an error' do
+        expect {
+          Incr::Service::Version.increment_segment(version, 'patch')
+        }.to raise_error(NoMethodError)
+      end
+    end
+
+    context 'with an unknown segment' do
+      let(:version) { SemVersion.new('1.0.0') }
+
+      it 'should not do anything' do
+        expected = '1.0.0'
+        result = Incr::Service::Version.increment_segment(version, 'foobar')
+
+        expect(expected).to eql(result.to_s)
+      end
+    end
+  end
+end
